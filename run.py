63776469e0c8ed1173585563553eb377d96bd8d5
Agnieszka Lukasiak <lukasiak@me.com>
2024-07-25 11:37:02 +0000

    add get ford funtion, started play funtion

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-import
+import gspread
+from google.oauth2.service_account import Credetials
+import random
+from words import word_list
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+def play(word):
+    word_completion ="_" * len (word)
